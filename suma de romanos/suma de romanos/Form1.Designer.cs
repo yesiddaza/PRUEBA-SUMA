9dbe3e55b8976ba296a7408b883a8f3f1bfe32cc
yesiddaza <yiye_91@hotmail.com>
2015-02-16 11:47:34 -0500

    PRUEBA NUMEROS EN C#

---
+++ suma de romanos/suma de romanos/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace suma_de_romanos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.arabicNumber1 = new System.Windows.Forms.TextBox();
+            this.arabicNumber2 = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.romanAnswer = new System.Windows.Forms.Label();
+            this.arabicAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // arabicNumber1
+            // 
+            this.arabicNumber1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.arabicNumber1.Location = new System.Drawing.Point(15, 55);
+            this.arabicNumber1.MaxLength = 4;
+            this.arabicNumber1.Name = "arabicNumber1";
+            this.arabicNumber1.Size = new System.Drawing.Size(272, 31);
+            this.arabicNumber1.TabIndex = 0;
+            this.arabicNumber1.TextChanged += new System.EventHandler(this.arabicNumber1_TextChanged);
+            // 
+            // arabicNumber2
+            // 
+            this.arabicNumber2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.arabicNumber2.Location = new System.Drawing.Point(313, 55);
+            this.arabicNumber2.MaxLength = 4;
+            this.arabicNumber2.Name = "arabicNumber2";
+            this.arabicNumber2.Size = new System.Drawing.Size(272, 31);
+            this.arabicNumber2.TabIndex = 1;
+            this.arabicNumber2.TextChanged += new System.EventHandler(this.arabicNumber2_TextChanged);
+            // 
+            // addButton
+            // 
+            this.addButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addButton.Location = new System.Drawing.Point(212, 131);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(120, 45);
+            this.addButton.TabIndex = 2;
+            this.addButton.Text = "SUMAR";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // romanAnswer
+            // 
+            this.romanAnswer.AutoSize = true;
+            this.romanAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.romanAnswer.Location = new System.Drawing.Point(150, 211);
+            this.romanAnswer.Name = "romanAnswer";
+            this.romanAnswer.Size = new System.Drawing.Size(248, 25);
+            this.romanAnswer.TabIndex = 3;
+            this.romanAnswer.Text = "(Respuesta en romanos)";
+            this.romanAnswer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // arabicAnswer
+            // 
+            this.arabicAnswer.AutoSize = true;
+            this.arabicAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.arabicAnswer.Location = new System.Drawing.Point(150, 251);
+            this.arabicAnswer.Name = "arabicAnswer";
+            this.arabicAnswer.Size = new System.Drawing.Size(248, 25);
+            this.arabicAnswer.TabIndex = 4;
+            this.arabicAnswer.Text = "(Respuesta en arábigos)";
+            this.arabicAnswer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.arabicAnswer.Click += new System.EventHandler(this.arabicAnswer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(597, 334);
+            this.Controls.Add(this.arabicAnswer);
+            this.Controls.Add(this.romanAnswer);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.arabicNumber2);
+            this.Controls.Add(this.arabicNumber1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox arabicNumber1;
+        private System.Windows.Forms.TextBox arabicNumber2;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Label romanAnswer;
+        private System.Windows.Forms.Label arabicAnswer;
+    }
+}
+
---
+++ suma de romanos/suma de romanos/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace suma_de_romanos
+{
+    public partial class Form1 : Form
+    {
+        int arabicValue1 = 0, arabicValue2 = 0, arabicSum = 0;
+        String romanAnswerValue;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool isNumber(String inboxText)
+        {
+            try
+            {
+                decimal newDecimal = Convert.ToDecimal(inboxText);
+                return true;
+            }
+            catch 
+            {
+                return false;
+            }
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            if ((isNumber(arabicNumber2.Text)) && (isNumber(arabicNumber2.Text)))
+            {
+                
+                arabicValue1 = Convert.ToInt16(arabicNumber1.Text);
+                arabicValue2 = Convert.ToInt16(arabicNumber2.Text);
+                arabicSum = arabicValue1 + arabicValue2;
+                if (arabicSum < 4000)
+                {
+                    romanAnswerValue = convertToRomanNumber(arabicSum);
+                    romanAnswer.Text = romanAnswerValue;
+                    arabicAnswer.Text = Convert.ToString(arabicSum);
+                }
+                else
+                    MessageBox.Show("La respuesta debe ser menor a 4000", "", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else            
+                MessageBox.Show("Solo se pueden ingresar números", "", MessageBoxButtons.OK, MessageBoxIcon.Error);            
+        }
+        
+        public void resetElements()
+        {
+            romanAnswer.Text = "(Respuesta en romanos)";
+            arabicAnswer.Text = "(Respuesta en arábigos)";      
+        }
+
+        public String convertToRomanNumber(int arabicNumber)
+        {
+            String[] unitArray = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+            String[] tenArray = { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+            String[] hundredArray = { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
+            String[] thousandUnitArray = { "", "M", "MM", "MMM"};
+            int thousandUnit = arabicNumber / 1000;
+            int hundred = (arabicNumber % 1000) / 100;
+            int ten = ((arabicNumber % 1000) % 100) / 10;
+            int unit = (((arabicNumber % 1000) % 100) % 10);
+            if (arabicNumber >= 1000)
+                return thousandUnitArray[thousandUnit] + hundredArray[hundred] + tenArray[ten] + unitArray[unit];
+            else if (arabicNumber >= 100)
+                return hundredArray[hundred] + tenArray[ten] + unitArray[unit];
+            else if (arabicNumber >= 10)
+                return tenArray[ten] + unitArray[unit];
+            else
+                return unitArray[unit];
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            resetElements();
+        }
+
+        private void arabicNumber1_TextChanged(object sender, EventArgs e) { }
+
+        private void arabicNumber2_TextChanged(object sender, EventArgs e) { }
+
+        private void arabicAnswer_Click(object sender, EventArgs e) { }
+    }
+}
